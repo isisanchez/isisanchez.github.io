638b87e43fe0d998faf573346ba2b0ea9d870810
isisanchez <86503091+isisanchez@users.noreply.github.com>
2021-07-08 19:51:32 -0400

    trabajo 2
    trabajo completo

---
+++ commlab/semana2/js/script.js
@@ -0,0 +1,51 @@
+// function cambioderecha(){
+// document.getElementById('photo'). src = "imagenes/imagen4.png"
+//
+// }
+//
+//
+// function cambioizquerda(){
+// document.getElementById('photo'). src = "imagenes/imagen1.png"
+//
+// }
+// document.getElementById('photo').addEventListener("mouseenter",()=>{
+//   document.getElementById('photo'). src="imagenes/imagen1"
+
+
+function cambiofinal() {
+  console.log(document);
+  document.getElementById('foto10'). src = "imagenes/10.png";
+document.getElementById('fotocambiar'). innerHTML="Marcianito finalmente se sacó un 3.0, más suerte la próxima Marcianito!";
+
+}
+function cambiofinal2() {
+  console.log(document);
+  document.getElementById('foto10'). src = "imagenes/11.png";
+document.getElementById('fotocambiar'). innerHTML="La buena suerte no necesita explicación, Marcianito!";
+
+}
+
+
+
+
+
+//
+// document.getElementById('titulo1').innerHTML = "commlab";
+// function miFuncion() {
+// console.log("hola");
+// console.log(document);
+// console.log(document.getElementById('titulo'));
+// document.getElementById('titulo1').innerHTML = "Hola jenni";
+// document.getElementById('titulo1'). style.color = "magenta";
+// document.getElementById('titulo1'). style.backgroundColor = "yellow";
+// document.getElementsByClassName('box')[0]. style.backgroundColor= "yellow"
+// document.getElementsByClassName('box')[1]. style.backgroundColor= "magenta"
+// }
+// function miFuncion2() {
+// console.log("otra vez");
+// console.log(document);
+// console.log(document.getElementById('titulo1'));
+// document.getElementById('titulo1'). innerHTML= "chao jenni"
+// document.getElementById('titulo1'). style.color = "black";
+//
+// }
