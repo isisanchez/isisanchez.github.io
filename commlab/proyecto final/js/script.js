1e98db475f11f6c692f2d0e417648ec0effa496e
isisanchez <isanchez18@alumnos.utalca.cl>
2021-08-12 19:03:56 -0400

    proyecto final
    nuevo trabajo

---
+++ commlab/proyecto final/js/script.js
@@ -0,0 +1,65 @@
+
+document.getElementById('photo').addEventListener("mouseenter",()=>{
+  document.getElementById('photo').src = "imagenes/medios.izquerda.jpg";
+
+
+  })
+
+  document.getElementById('photo').addEventListener("mouseleave",()=>{
+    document.getElementById('photo').src = "imagenes/medios.centrado.jpg";
+
+
+    })
+
+    document.getElementById('crucepro1').addEventListener("mouseenter",()=>{
+      document.getElementById('crucepro1').src = "imagenes/crucepro1.jpg";
+
+
+      })
+
+      document.getElementById('crucepro1').addEventListener("mouseleave",()=>{
+        document.getElementById('crucepro1').src = "imagenes/cruce1.jpg";
+
+
+        })
+
+        document.getElementById('crucepro2').addEventListener("mouseenter",()=>{
+          document.getElementById('crucepro2').src = "imagenes/crucepro2.jpg";
+
+
+          })
+
+          document.getElementById('crucepro2').addEventListener("mouseleave",()=>{
+            document.getElementById('crucepro2').src = "imagenes/cruce2.jpg";
+
+            })
+
+
+      
+            function commentBox(){
+            	var name=document.getElementById('name').value;
+            	var comment=document.getElementById('comment').value;
+
+            	if(name =="" || comment ==""){
+            		alert("Porfavor introduce la informacion requerida!");
+            	}else{
+            		var parent=document.createElement('div');
+            		var el_name=document.createElement('h5');
+            		var el_message=document.createElement('p');
+            		var el_line=document.createElement('hr');
+            		var txt_name=document.createTextNode(name);
+            		var txt_message=document.createTextNode(comment);
+            		el_name.appendChild(txt_name);
+            		el_message.appendChild(txt_message);
+            		el_line.style.border='1px solid #000';
+            		parent.appendChild(el_name);
+            		parent.appendChild(el_line);
+            		parent.appendChild(el_message);
+            		parent.setAttribute('class', 'pane');
+            		document.getElementById('result').appendChild(parent);
+
+            		document.getElementById('name').value="";
+            		document.getElementById('comment').value="";
+            	}
+
+            }
